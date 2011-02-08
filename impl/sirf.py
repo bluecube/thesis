4f4fbb2423872b93f495b62f15374b0ccea5acc6
Kuba Marek <blue.cube@seznam.cz>
2011-02-09 00:09:02 +0100

    Parsing a sirf message. Something is slightly wrong.

--- impl/sirf.py
+++ impl/sirf.py
@@ -1,8 +1,16 @@
 import time
 import struct
+import logging
+
 import serial_wrapper
+import sirf_messages
+
+_logger = logging.getLogger('localization.gps')
 
 class SirfMessageError(Exception):
+    pass
+
+class UnrecognizedMessageException(Exception):
     pass
 
 def read_message(serial):
@@ -28,7 +36,7 @@ def read_message(serial):
         
         message_ending = serial.read_with_timeout(2, end_time)
         if message_ending != bytes([0xB0, 0xB3]):
-            raise SirfMessageError('Invalid message end sequence')
+            raise SirfMessageError('Invalid message end sequence.')
 
         return data
     except serial_wrapper.SerialWrapperTimeout:
@@ -51,3 +59,38 @@ def send_message(serial, data):
     serial.write(data)
     serial.write(bytes([checksum >> 8, checksum & 0xFF, 0xB0, 0xB3]))
     serial.flush()
+
+
+def from_bytes(data):
+    """
+    Factory method that returns an sirf message object from its
+    binary representation.
+    data: Payload of the sirf message.
+    """
+
+    message_id = data[0]
+
+    _logger.debug("Message ID: " + str(message_id) + ".")
+
+    if not message_id in message_types:
+        raise UnrecognizedMessageException("Unrecognized message " +
+            str(message_id) + ".")
+
+    klass = message_types[message_id]
+
+    output = klass.from_bytes(data)
+    #assert output.message_id == message_id
+
+    return output
+
+# Enumeration of all available messages:
+message_types = {}
+for v in vars(sirf_messages).values():
+    try:
+        if v != sirf_messages._SirfMessage and \
+            issubclass(v, sirf_messages._SirfMessage):
+
+            message_types[v.get_message_id()] = v
+    except TypeError:
+        pass
+

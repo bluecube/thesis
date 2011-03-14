98c8c908e7da746b4cb448cd56c981e079e0e842
Kuba Marek <blue.cube@seznam.cz>
2011-03-14 12:53:04 +0100

    Some updates in the errors code. Only selecting a single measurement block.

--- impl/errors.py
+++ impl/errors.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import numpy
 import math
+import argparse
 
 import gps.gps_replay
 
@@ -50,11 +51,10 @@ class Measurement:
         
         return True
 
-    def clock_offset(self, receiver_pos):
-        return (self.pseudorange - self.iono_delay -
-            self.geom_range(receiver_pos)) / C
+    def clock_offset(self):
+        return (self.pseudorange - self.iono_delay - self.geom_range()) / C
         
-    def geom_range(self, receiver_pos):
+    def geom_range(self):
         distance = self.sv_pos - receiver_pos
         return math.sqrt(distance * distance.T)
         
@@ -76,7 +76,7 @@ def measurement_generator():
     Yields measurements which are used in passes 2 and 3.
     """
 
-    replay = gps.gps_replay.GpsReplay(sys.argv[1])
+    replay = gps.gps_replay.GpsReplay(arguments.recording)
     sv = {}
 
     group = []
@@ -105,41 +105,44 @@ def measurement_generator():
 def pass_one():
     logger.info("Pass 1: Estimate receiver position.")
 
-    replay = gps.gps_replay.GpsReplay(sys.argv[1])
+    replay = gps.gps_replay.GpsReplay(arguments.recording)
 
     count = 0
     receiver_pos = numpy.matrix([[0., 0., 0.]])
 
-    for msg in replay:
-        if isinstance(msg, MeasureNavigationDataOut):
-            count += 1
-            receiver_pos += msg.pos
+    try:
+        for msg in replay:
+            if isinstance(msg, MeasureNavigationDataOut):
+                count += 1
+                receiver_pos += msg.pos
+    except KeyboardInterrupt:
+        logger.info("Ok, that should be enough.")
 
     receiver_pos /= count
     logger.debug("Found " + str(count) + " messages, average position is " + str(receiver_pos) + ".")
 
     return receiver_pos
 
-def get_drift(m1, m2, receiver_pos):
+def get_drift(m1, m2):
     """
     Calculate clock drift between two measurement groups.
     """
     if m1.time == m2.time:
         return 0
 
-    drift = m2.clock_offset(receiver_pos) - m1.clock_offset(receiver_pos)
+    drift = m2.clock_offset() - m1.clock_offset()
     drift /= m2.time - m1.time
 
     return drift
 
-def is_clock_correction(m1, m2, receiver_pos):
+def is_clock_correction(m1, m2):
     """
     Returns true if there was a clock correction between the 
     two measurements.
     """
-    return abs(get_drift(m1, m2, receiver_pos)) > MAX_CLOCK_DRIFT
+    return abs(get_drift(m1, m2)) > MAX_CLOCK_DRIFT
 
-def pass_two(receiver_pos):
+def pass_two():
     """
     Splits the measurements into blocks between clock corrections,
     calculates the least squares on the blocks and calls pass
@@ -158,9 +161,36 @@ def pass_two(receiver_pos):
     r = 0
     s = 0
 
+    if not (arguments.group is None):
+        last = generator.__next__()
+
+        for i in range(arguments.group):
+            for measurement in generator:
+                if is_clock_correction(last, measurement):
+                    last = measurement
+                    block = [measurement]
+                    break
+                else:
+                    last = measurement
+
+        block = [last]
+    else:
+        block = [generator.__next__()]
+
+
     for measurement in generator:
-        if len(block) > 0 and is_clock_correction(measurement, block[-1], receiver_pos):
-            pass_three(block, receiver_pos, p, q, r, s)
+        offset = block[-1].clock_offset()
+        time = block[-1].time - offset
+        p += offset * time
+        q += offset
+        r += time
+        s += time * time
+
+        if is_clock_correction(block[-1], measurement):
+            pass_three(block, p, q, r, s)
+
+            if not (arguments.group is None):
+                return
 
             # reset it all.
             block = []
@@ -171,18 +201,16 @@ def pass_two(receiver_pos):
 
         block.append(measurement)
 
-        offset = measurement.clock_offset(receiver_pos)
-        time = measurement.time - offset
+    offset = block[-1].clock_offset()
+    time = block[-1].time - offset
+    p += offset * time
+    q += offset
+    r += time
+    s += time * time
 
-        p += offset * time
-        q += offset
-        r += time
-        s += time * time
+    pass_three(block, p, q, r, s)
 
-    
-    pass_three(block, receiver_pos, p, q, r, s)
-
-def pass_three(block, receiver_pos, p, q, r, s):
+def pass_three(block, p, q, r, s):
     """
     Do the main error calculations.
     At this place we know both the physical position of the receiver
@@ -199,36 +227,45 @@ def pass_three(block, receiver_pos, p, q, r, s):
     print("  b:     ", b)
     print("  start: ", block[0].time)
     print("  end:   ", block[-1].time)
+    print("  a * C: ", a * C)
+    print("  b * C: ", b * C)
 
     # convert the a and b to calculate clock offset from receiver sw time instead
     # of gps system time.
-    b /= (1 + a)
-    a /= (1 + a)
+    #b /= (1 + a)
+    #a /= (1 + a)
 
     for measurement in block:
         clock_offset_equiv = C * (a * measurement.time + b)
         corrected_pseudorange = measurement.pseudorange - measurement.iono_delay
+
+        print(measurement.time, clock_offset_equiv, file=arguments.datapoints)
     
         corrected_pseudorange -= clock_offset_equiv
 
-        error = corrected_pseudorange - measurement.geom_range(receiver_pos)
+        error = corrected_pseudorange - measurement.geom_range()
 
-        print("Error is " + str(error) + " meters")
+        #print("Error is " + str(error) + " meters")
         
 setup_logging()
 
 logger = logging.getLogger('main')
 
-logger.info("Calculate the UERE from recorded data.")
-logger.info("Assumes that the receiver was stationary during whole recording.")
+arg_parser = argparse.ArgumentParser(
+    description="Calculate the UERE from recorded data.\n"
+    "Assumes that the receiver was stationary during whole recording.")
+arg_parser.add_argument('recording')
+arg_parser.add_argument('--group', default=None, type=int,
+    help="Work only with the given group in phase 3. For testing only.")
+arg_parser.add_argument('--datapoints', default=open("/dev/null", "w"),
+    type=argparse.FileType("w"),
+    help="File into which the data points in phase 3 will go.")
 
-if len(sys.argv) != 2:
-    logger.error("Usage: " + sys.argv[0] + " <recording>")
-    sys.exit(1)
+arguments = arg_parser.parse_args()
 
+receiver_pos = pass_one()
 try:
-    pos = pass_one()
-    pass_two(pos)
+    pass_two()
 except KeyboardInterrupt:
     logger.info("Terminating.")
     exit(0)

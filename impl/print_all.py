4aaebbd21d9dd1b412c3716160a460ef150a3c98
Kuba Marek <blue.cube@seznam.cz>
2011-03-26 21:52:26 +0100

    Filtering for the message printer.

--- impl/print_all.py
+++ impl/print_all.py
@@ -3,6 +3,7 @@
 import gps
 import logging
 import sys
+import argparse
 
 from gps.sirf_messages import *
 
@@ -17,14 +18,22 @@ setup_logging()
 logger = logging.getLogger('main')
 logger.setLevel(logging.DEBUG)
 
-if len(sys.argv) != 2:
-    logger.error("Usage: " + sys.argv[0] + " <port or recording>")
-    sys.exit(1)
+arg_parser = argparse.ArgumentParser(
+    description="Calculate the UERE from recorded data.\n"
+    "Assumes that the receiver was stationary during whole recording.")
 
-x = gps.open_gps(sys.argv[1])
+arg_parser.add_argument('gps',
+    help="Gps port or recording.")
+arg_parser.add_argument('--filter', default=None, type=int,
+    help="Only print messages with this ID.")
+arguments = arg_parser.parse_args()
+
+x = gps.open_gps(arguments.gps)
 
 try:
     for msg in x:
+        if arguments.filter is not None and msg.message_id != arguments.filter:
+            continue
         print(msg)
         print()
 except KeyboardInterrupt:

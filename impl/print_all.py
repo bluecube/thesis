8f733bb0f0082f9dfed31dd7c8a2bdedbed22141
Kuba Marek <blue.cube@seznam.cz>
2011-05-05 22:55:56 +0200

    Separators in print_all.py

--- impl/print_all.py
+++ impl/print_all.py
@@ -26,6 +26,8 @@ arg_parser.add_argument('gps',
     help="Gps port or recording.")
 arg_parser.add_argument('--filter', default=None, type=int,
     help="Only print messages with this ID.")
+arg_parser.add_argument('--no-separators', default=False, action='store_true',
+    help="Disable the display of separators in long pauses?")
 arguments = arg_parser.parse_args()
 
 x = gps.open_gps(arguments.gps)
@@ -33,10 +35,18 @@ x = gps.open_gps(arguments.gps)
 if arguments.filter is not None:
     x.set_message_rate(arguments, 1)
 
+last_time = float("nan")
+
 try:
     for msg in x:
         if arguments.filter is not None and msg.message_id != arguments.filter:
             continue
+
+        if x.last_msg_time - last_time > 0.3 and not arguments.no_separators:
+            print("***")
+            print()
+        last_time = x.last_msg_time
+
         print(msg)
         print()
 except KeyboardInterrupt:

f55f9ee83e8a130ed5780822df0ad0614202fbba
Kuba Marek <blue.cube@seznam.cz>
2011-10-07 12:28:28 +0200

    Calculate CRC of recorded messages.

---
+++ impl/checksum.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import gps
+import logging
+import sys
+import argparse
+from zlib import crc32
+
+from gps.sirf_messages import *
+
+def setup_logging():
+    logging.basicConfig(
+        format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level = logging.INFO
+    )
+
+setup_logging()
+
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+
+arg_parser = argparse.ArgumentParser(
+    description="Calculate crc32 checksum from recorded messages' payloads.")
+
+arg_parser.add_argument('gps',
+    help="Gps port or recording.")
+arguments = arg_parser.parse_args()
+
+x = gps.open_gps(arguments.gps)
+
+checksum = crc32(b'')
+
+try:
+    while True:
+        checksum = crc32(x._read_binary_sirf_msg(), checksum)
+except EOFError:
+    pass
+
+print("checksum: {:#08x}".format(checksum))

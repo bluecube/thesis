48c0f2ee8da0bd111cf7d5de3d62255edbf22151
Kuba Marek <blue.cube@seznam.cz>
2013-02-11 16:14:00 +0100

    Cleaned up makefile.

--- text/plots/hdop-hist.sh
+++ text/plots/hdop-hist.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"/../../impl/
-$DIR/compare_hdop_histogram.py \
-	$DIR/recordings/processed_numpy_arrays/month_wgs84.npy \
-	$DIR/recordings/roboauto_merged.recording2 \
+$IMPL_DIR/compare_hdop_histogram.py \
+	$IMPL_DIR/recordings/processed_numpy_arrays/month_wgs84.npy \
+	$IMPL_DIR/recordings/roboauto_merged.recording2 \
 	--label '"Month" dataset','"Roboauto" dataset' \
 	--max-plot-hdop 10 --save-hist-plot $1 --no-show
--- text/plots/wgs84-hdop-error.sh
+++ text/plots/wgs84-hdop-error.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"/../../impl/
-$DIR/wgs84_errors.py \
-	$DIR/recordings/processed_numpy_arrays/month_wgs84.npy \
+$IMPL_DIR/wgs84_errors.py \
+	$IMPL_DIR/recordings/processed_numpy_arrays/month_wgs84.npy \
 	--plotted-sample-count 10000 --max-plot-hdop 4 --max-plot-error 20 \
 	--save-hdop-plot $1 --no-show

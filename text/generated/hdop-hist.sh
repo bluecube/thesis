4d8503db717e97f1cfefd585a1aa65b90f4ea435
Kuba Marek <blue.cube@seznam.cz>
2013-04-09 14:54:53 +0200

    Moving the data around.

--- text/generated/hdop-hist.sh
+++ text/generated/hdop-hist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 $IMPL_DIR/compare_hdop_histogram.py \
-	$IMPL_DIR/recordings/processed_numpy_arrays/month_wgs84.npy \
-	$IMPL_DIR/recordings/roboauto_merged.recording2 \
+	$IMPL_DIR/../data/preprocessed/month_wgs84.npy \
+	$IMPL_DIR/../data/roboauto_merged.recording2 \
 	--label '"Month" dataset','"Roboauto" dataset' \
 	--max-plot-hdop 10 --save-hist-plot $1 --no-show
--- text/generated/wgs84-hdop-error.sh
+++ text/generated/wgs84-hdop-error.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 $IMPL_DIR/wgs84_errors.py \
-	$IMPL_DIR/recordings/processed_numpy_arrays/month_wgs84.npy \
+	$IMPL_DIR/../data/preprocessed/month_wgs84.npy \
 	--plotted-sample-count 10000 --max-plot-hdop 4 --max-plot-error 20 \
 	--save-hdop-plot $1 --no-show
